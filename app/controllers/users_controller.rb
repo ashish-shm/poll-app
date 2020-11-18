e9dc729363c3b9dc25a2e973a90f93df57d48ef7
Ashish Sharma <ashish.shm09@gmail.com>
2020-11-18 06:28:12 +0530

    Refactor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      
-    end
+    log_in @user if @user.save
   end
 
   private
