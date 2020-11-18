3832f17becc934767ad7ff679fde032cf06b4360
Ashish Sharma <ashish.shm09@gmail.com>
2020-11-18 06:57:43 +0530

    Added login feature

--- app/javascript/components/LoginPage.js
+++ app/javascript/components/LoginPage.js
@@ -1,11 +1,54 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function LoginPage() {
+    const [inputs, setInputs] = useState({});
+    let url = "http://localhost:3000/login"
+    const headers = {
+        "Content-Type": "application/json",
+        "X-CSRF-TOKEN": document.querySelector('[name="csrf-token"]').content,
+    }
+
+    const handleInputChange = (event) => {
+        event.persist();
+        setInputs(inputs => ({ ...inputs, [event.target.name]: event.target.value }));
+    }
+
+    const handleSubmit = (event) => {
+        if (event) {
+            event.preventDefault();
+            fetch(url, {
+                method: "POST",
+                headers: headers,
+                body: JSON.stringify({ session: inputs }),
+            })
+                .then((res) => {
+                    if (res.status == 200)
+                        (window.location.href = '/')
+                })
+                .catch((err) => setError(err));
+        }
+    }
     return (
-        <div>
-            <h1>Login to your account</h1>
-        </div>
-    )
+        <>
+
+            <form onSubmit={handleSubmit}>
+
+                <div>
+                    <label>Email Address</label>
+                    <input type="email" name="email" onChange={handleInputChange} value={inputs.email} required />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="password" name="password" onChange={handleInputChange} value={inputs.password} />
+                </div>
+
+                <button type="submit">Sign In</button>
+            </form>
+
+
+
+        </>
+    );
 }
 
-export default LoginPage
+export default LoginPage;
