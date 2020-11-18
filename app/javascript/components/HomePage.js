70b73e417120912ff5a594d290be4b2ef1cbe280
Ashish Sharma <ashish.shm09@gmail.com>
2020-11-19 05:27:34 +0530

    Added styling for polls

--- app/javascript/components/HomePage.js
+++ app/javascript/components/HomePage.js
@@ -10,7 +10,7 @@ function HomePage(props) {
                 <p className='hero-text'>Welcome to Poll App!</p>
                 {loggedInUser ?
                     <><a className='link' href='/polls/new'>Create a poll!</a>
-                        <a className='link' href='/login'>Cast a vote!</a></>
+                        <a className='link' href='/polls'>Cast a vote!</a></>
                     : <p className='hero-text'> <a className='hero-link' href='/login'> Login </a>/<a className='hero-link' href='/register'> Register </a> to start casting votes on the Poll-App</p>}
 
             </div>
--- app/javascript/components/Polls.js
+++ app/javascript/components/Polls.js
@@ -7,6 +7,23 @@ function Polls(props) {
     return (
         <>
             <Header loggedInUser={loggedInUser} />
+            <div className='polls-container'>
+                <h1 className='poll-heading'>All Polls</h1>
+                {polls?.map(poll => {
+                    return (
+                        <div className='poll-box'>
+                            <h1 className='poll-no'>Poll No:{poll.id}</h1>
+                            <h1 className='poll-question'>{poll.question}</h1>
+                            <ul className='flex'>
+                                <li key={'option1'}><button className='vote-btn'>{poll.option1}</button></li>
+                                <li key={'option2'}><button className='vote-btn'>{poll.option1}</button></li>
+                                <li key={'option3'}><button className='vote-btn'>{poll.option1}</button></li>
+                                <li key={'option4'}><button className='vote-btn'>{poll.option1}</button></li>
+                            </ul>
+                        </div>
+                    )
+                })}
+            </div>
         </>
     )
 }
