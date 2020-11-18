54e14dea3178eeba60d8ebee49ccc2a373cde03e
Ashish Sharma <ashish.shm09@gmail.com>
2020-11-18 08:07:06 +0530

    UI changes

--- app/javascript/components/HomePage.js
+++ app/javascript/components/HomePage.js
@@ -2,12 +2,17 @@ import React from 'react'
 import Header from './Header'
 
 function HomePage(props) {
+    let { loggedInUser } = props
     return (
         <>
-            <Header loggedInUser={props.loggedInUser} />
+            <Header loggedInUser={loggedInUser} />
             <div className='hero-section'>
-                <p className='hero-text'>Welcome to Polls App!</p>
-                <p className='hero-text'> Login/Register to start casting votes on the Poll-App</p>
+                <p className='hero-text'>Welcome to Poll App!</p>
+                {loggedInUser ?
+                    <><a className='link' href='/login'>Create a poll!</a>
+                        <a className='link' href='/login'>Cast a vote!</a></>
+                    : <p className='hero-text'> <a className='hero-link' href='/login'> Login </a>/<a className='hero-link' href='/register'> Register </a> to start casting votes on the Poll-App</p>}
+
             </div>
         </>
     )
