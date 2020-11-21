b1bd970e135af5f5058f53f10b241dd7dbcb0970
Ashish Sharma <ashish.shm09@gmail.com>
2020-11-22 03:17:00 +0530

    Prettier changes

--- app/javascript/components/AuthHeader.js
+++ app/javascript/components/AuthHeader.js
@@ -1,32 +1,35 @@
-import React from 'react'
+import React from "react";
 
 function AuthHeader(props) {
-    let url = "http://localhost:3000/logout"
-    const headers = {
-        "Content-Type": "application/json",
-        "X-CSRF-TOKEN": document.querySelector('[name="csrf-token"]').content,
-    }
-    const handleClick = () => {
-        fetch(url, {
-            method: "DELETE",
-            headers: headers,
-        })
-            .then((res) => {
-                if (res.status == 204)
-                    (window.location.href = '/')
-            })
-            .catch((err) => setError(err));
+  let url = "http://localhost:3000/logout";
+  const headers = {
+    "Content-Type": "application/json",
+    "X-CSRF-TOKEN": document.querySelector('[name="csrf-token"]').content,
+  };
 
-    }
-    return (
-        <div className='flex'>
-            <a className='heading' href='/'>Poll-App</a>
-            <nav className='flex'>
-                <h1 className='link'>{props.loggedInUser}</h1>
-                <h1 className='link' onClick={handleClick}>Logout</h1>
-            </nav>
-        </div>
-    )
+  const handleClick = () => {
+    fetch(url, {
+      method: "DELETE",
+      headers: headers,
+    })
+      .then((res) => {
+        if (res.status == 204) window.location.href = "/";
+      })
+      .catch((err) => setError(err));
+  };
+  return (
+    <div className="flex">
+      <a className="heading" href="/">
+        Poll-App
+      </a>
+      <nav className="flex">
+        <h1 className="link">{props.loggedInUser}</h1>
+        <h1 className="link" onClick={handleClick}>
+          Logout
+        </h1>
+      </nav>
+    </div>
+  );
 }
 
-export default AuthHeader
+export default AuthHeader;
--- app/javascript/components/NonAuthHeader.js
+++ app/javascript/components/NonAuthHeader.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React from "react";
 
 function NonAuthHeader() {
-    return (
-        <div className='flex'>
-            <a className='heading' href='/'>Poll-App</a>
-            <nav className='flex'>
-                <a className='link' href='/login'>Login</a>
-                <a className='link' href='/register'>Register</a>
-
-            </nav>
-        </div>
-    )
+  return (
+    <div className="flex">
+      <a className="heading" href="/">
+        Poll-App
+      </a>
+      <nav className="flex">
+        <a className="link" href="/login">
+          Login
+        </a>
+        <a className="link" href="/register">
+          Register
+        </a>
+      </nav>
+    </div>
+  );
 }
 
-export default NonAuthHeader
+export default NonAuthHeader;
