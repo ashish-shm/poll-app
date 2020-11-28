eda896a19e1fe7d7f9d47739399cc9e72e7498f6
Ashish Sharma <ashish.shm09@gmail.com>
2020-11-28 20:36:47 +0530

    Changed to root route in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # get 'polls/index'
   # get 'polls/create'
   # get 'polls/new'
-  get '/', to: 'home#index'
+  root 'home#index'
   get '/login', to: 'session#new'
   post '/login', to: 'session#create'
   delete '/logout', to: 'session#destroy'
